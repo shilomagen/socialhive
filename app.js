8cc7f485a36ef3d4626e2f4024b4012f0a9789d1
shilomangam <smangam@gmail.com>
2016-08-19 12:26:48 +0300

    model and post examples

--- app.js
+++ app.js
@@ -10,23 +10,37 @@ var morgan = require('morgan');
 var jwt = require('jwt-simple');
 var port = process.env.PORT || 3000;
 
-
+mongoose.connect('mongodb://localhost/socialhive', function() {
+	console.log("DB connected successfully");
+});
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(passport.initialize());
 
-var obj = {
-	shilo:"hello",
-	age:27
-};
+var User = require("./models/user");
 
-app.get('/events/:id', function(req,res){
-	res.send(obj);
+app.post('/createUser', function(req, res){
+	var userToInsert = new User();
+	console.log(req.body.name + " " + req.body.username + " " + req.body.password);
+	userToInsert.name = req.body.name;
+	userToInsert.username = req.body.username;
+	userToInsert.password = req.body.password;
+
+	userToInsert.save(function(err){
+		if (err){
+
+			res.send(err);
+
+		} else {
+			res.send(userToInsert.name + " Was created successfully on DB");
+		}
+	})
 });
 
+
 app.listen(port);
-console.log("Server listening on port "+ port);
+console.log("Server listening on port " + port);
