c568b66fbfcaa38146414566de122f80359d1abe
shilomangam <smangam@gmail.com>
2016-09-10 16:10:52 +0300

    shilo changes

--- public/scripts/socialhive.js
+++ public/scripts/socialhive.js
@@ -14,43 +14,39 @@ $(document).ready(function(){
     $("#successContainer").hide();
 
 
-
 });
-
 var eventID;
 
-
-
 $("#createEventButton").click(function() {
-    $("#overviewContent").hide();
-    $("#userProfile").hide();
-    $("#eventManagerYourEvents").hide();
-    $("#eventManagerInvitedEvents").hide();
-    $("#eventInfo").hide();
-    $("#createEvent").fadeIn(600);
-    $("#addParticipants").fadeIn(600);
+	$("#overviewContent").hide();
+	$("#userProfile").hide();
+	$("#eventManagerYourEvents").hide();
+	$("#eventManagerInvitedEvents").hide();
+	$("#eventInfo").hide();
+	$("#createEvent").fadeIn(600);
+	$("#addParticipants").fadeIn(600);
 });
 $("#overviewContentButton").click(function() {
-    $("#createEvent").hide();
-    $("#addParticipants").hide();
-    $("#eventManager").hide();
-    $("#userProfile").hide();
-    $("#eventManagerYourEvents").hide();
-    $("#eventManagerInvitedEvents").hide();
-    $("#eventInfo").hide();
-    $("#overviewContent").fadeIn(600);
+	$("#createEvent").hide();
+	$("#addParticipants").hide();
+	$("#eventManager").hide();
+	$("#userProfile").hide();
+	$("#eventManagerYourEvents").hide();
+	$("#eventManagerInvitedEvents").hide();
+	$("#eventInfo").hide();
+	$("#overviewContent").fadeIn(600);
 
 
 });
 
 $("#eventManagerButton").click(function() {
-    $("#overviewContent").hide();
-    $("#addParticipants").hide();
-    $("#createEvent").hide();
-    $("#userProfile").hide();
-    $("#eventManagerYourEvents").fadeIn(600);
-    $("#eventManagerInvitedEvents").fadeIn(600);
-    $("#eventInfo").fadeIn(600);
+	$("#overviewContent").hide();
+	$("#addParticipants").hide();
+	$("#createEvent").hide();
+	$("#userProfile").hide();
+	$("#eventManagerYourEvents").fadeIn(600);
+	$("#eventManagerInvitedEvents").fadeIn(600);
+	$("#eventInfo").fadeIn(600);
 
 
 
@@ -64,15 +60,25 @@ $("#eventManagerButton").click(function() {
                 for (var i = 0; i < allEvents.length; ++i) {
                     showSingleEvent(allEvents[i])
                 }
+	$.ajax({
+		url: '/api/events',
+		method: 'get',
+		success: function(obj) {
+			obj.success = true;
+			if (obj.success) {
+				var allEvents = obj.msg;
+				for (var i = 0; i < allEvents.length; ++i) {
+					showSingleEvent(allEvents[i])
+				}
 
-            } else {
-                $('#msgFromServer').text(obj.msg);
-            }
-        },
-        error: function(obj) {
-            $('#msgFromServer').text(obj.msg);
-        }
-    });
+			} else {
+				$('#msgFromServer').text(obj.msg);
+			}
+		},
+		error: function(obj) {
+			$('#msgFromServer').text(obj.msg);
+		}
+	});
 
 
     $.ajax({
@@ -85,61 +91,68 @@ $("#eventManagerButton").click(function() {
                 for (var i = 0; i < allEvents.length; ++i) {
                     showSingleInvitedEvent(allEvents[i])
                 }
+	$.ajax({
+		url: '/api/events/invited',
+		method: 'get',
+		success: function(obj) {
+			obj.success = true;
+			if (obj.success) {
+				var allEvents = obj.msg;
+				for (var i = 0; i < allEvents.length; ++i) {
+					showSingleInvitedEvent(allEvents[i])
+				}
 
-            } else {
-                $('#msgFromServer').text(obj.msg);
-            }
-        },
-        error: function(obj) {
-            $('#msgFromServer').text(obj.msg);
-        }
-    });
+			} else {
+				$('#msgFromServer').text(obj.msg);
+			}
+		},
+		error: function(obj) {
+			$('#msgFromServer').text(obj.msg);
+		}
+	});
 
 
 
 });
 
 $("#userProfileButton").click(function() {
-    $("#overviewContent").hide();
-    $("#addParticipants").hide();
-    $("#createEvent").hide();
-    $("#eventManagerYourEvents").hide();
-    $("#eventManagerInvitedEvents").hide();
-    $("#eventInfo").hide();
-    $("#eventManager").hide();
-    $("#userProfile").fadeIn(600);
+	$("#overviewContent").hide();
+	$("#addParticipants").hide();
+	$("#createEvent").hide();
+	$("#eventManagerYourEvents").hide();
+	$("#eventManagerInvitedEvents").hide();
+	$("#eventInfo").hide();
+	$("#eventManager").hide();
+	$("#userProfile").fadeIn(600);
 
 });
 
 $("#createEventButtonNext").click(function(e) {
-    var eventToCreate = {
-        name: $('#eventName').val(),
-        startDate: $('#eventStartDate').val(),
-        numberOfParticipants: 10 // TODO: ADD PARCITIPANTS NUMBERS (?)
-    };
+	var eventToCreate = {
+		name: $('#eventName').val(),
+		startDate: $('#eventStartDate').val(),
+		numberOfParticipants: 10 // TODO: ADD PARCITIPANTS NUMBERS (?)
+	};
 
-    $.ajax({
-        url: '/api/events/create',
-        method: 'post',
-        data: eventToCreate,
-        success: function(obj) {
-            if (obj.success) {
-                eventID = obj.msg;
-                $("#createEvent").hide();
-                $("#addItemsToEvent").fadeIn(1500);
-               // moveToItemsDialog(eventID); // TODO: Open up a function with ajax call to create items (for loop that do ajax call for each item). after it do the same for partic.
-            } else {
-                $('#msgFromServer').text(obj.msg);
-            }
-        },
-        error: function(obj) {
-            $('#msgFromServer').text(obj.msg);
-        }
-    });
-
+	$.ajax({
+		url: '/api/events/create',
+		method: 'post',
+		data: eventToCreate,
+		success: function(obj) {
+			if (obj.success) {
+				eventID = obj.msg;
+				$("#createEvent").hide();
+				$("#addItemsToEvent").fadeIn(1500);
+				// moveToItemsDialog(eventID); // TODO: Open up a function with ajax call to create items (for loop that do ajax call for each item). after it do the same for partic.
+			} else {
+				$('#msgFromServer').text(obj.msg);
+			}
+		},
+		error: function(obj) {
+			$('#msgFromServer').text(obj.msg);
+		}
+	});
 });
-
-
 $("#createEventButtonNextToParticipants").click(function() {
    // var listOfItems = $('#itemsContainer');
    // var numOfItem = list.find('li').length;
@@ -173,6 +186,28 @@ $("#createEventButtonNextToParticipants").click(function() {
     $("#addParticipantsToEvent").fadeIn(1500);
 
 
+	var eventFullAds = "/api/events/" + eventID + "/addItem";
+	var listItems = $('#listItems').find('li');
+	for (var i = 0; i < listItems.length; ++i) {
+		var itemName = $(listItems[i]).find('span')[0].innerHTML;
+		$.ajax({
+			url: eventFullAds,
+			method: 'post',
+			data: {name: itemName},
+			success: function(obj) {
+				if (obj.success) {
+					console.log("Item added successfully");
+				} else {
+					$('#msgFromServer').text(obj.msg);
+				}
+			},
+			error: function(obj) {
+				$('#msgFromServer').text(obj.msg);
+			}
+		});
+	}
+	$("#addItemsToEvent").hide();
+	$("#addParticipantsToEvent").fadeIn(1500);
 });
 
 
@@ -192,22 +227,31 @@ $("#createEventButtonNextToFinish").click(function() {
             success: function(obj) {
                 if (obj.success) {
                     eventID = obj.msg;
+	var eventFullAds = "/api/events/" + eventID + "/addUser";
+	var listParticipants = $('#listParticipants').find('li');
+	for (var i = 0; i < listParticipants.length; ++i) {
+		var pEmail = $(listParticipants[i]).find('span')[0].innerHTML; //TODO: FIX THE NAME - WITHOUT SPAN
+		console.log(pEmail);
+		$.ajax({
+			url: eventFullAds,
+			method: 'post',
+			data: {email: pEmail},
+			success: function(obj) {
+				if (obj.success) {
+					eventID = obj.msg;
 
-                    // moveToItemsDialog(eventID); // TODO: Open up a function with ajax call to create items (for loop that do ajax call for each item). after it do the same for partic.
-                } else {
-                    $('#msgFromServer').text(obj.msg);
-                }
-            },
-            error: function(obj) {
-                $('#msgFromServer').text(obj.msg);
-            }
-        });
-
-    };
-    $("#addParticipantsToEvent").hide();
-    $("#successContainer").fadeIn(1500);
-
-
+					// moveToItemsDialog(eventID); // TODO: Open up a function with ajax call to create items (for loop that do ajax call for each item). after it do the same for partic.
+				} else {
+					$('#msgFromServer').text(obj.msg);
+				}
+			},
+			error: function(obj) {
+				$('#msgFromServer').text(obj.msg);
+			}
+		});
+	}
+	$("#addParticipantsToEvent").hide();
+	$("#successContainer").fadeIn(1500);
 });
 
 
@@ -224,63 +268,36 @@ function moveToItemsDialog(eventID){
     }
 }
 
-
-
-
-
-function onButtonClick_GetItemInfo()
-{
-    var nameOfItem = document.getElementById("itemInfo").value;
-
-    saveItem(nameOfItem);
+function saveItem() {
+	var itemName = $('#itemInfo').val();
+	var closeSpan = $('<span />').text('x').addClass('w3-closebtn w3-margin-right w3-large').on('click', function() {
+		$(this.parentElement).remove();
+	});
+	var textSpan = $('<span />').text(itemName).addClass('itemTextSpan');
+	var listItem = $('<li />').append(textSpan, closeSpan);
+	$('#listItems').append(listItem);
 }
 
-
-
-function saveItem(nameOfItem){
-    var closeSpan = $('<span />');
-    closeSpan.text('x').addClass('w3-closebtn w3-margin-right w3-large').on('click', function(){
-        this.parentElement.style.display='none';
-    });
-
-    var itemSpan =  $('<span>').text(nameOfItem);
-
-    var listItem = $('<li />').append(itemSpan, closeSpan);
-    $('#listItems').append(listItem);
-}
-
-
-
-
-function onButtonClick_GetParticipantInfo()
-{
-    var participantEmail = document.getElementById("participantEmail").value;
-
-    saveParticipant(participantEmail);
-}
-
-
-function saveParticipant(participantEmail){
-    var closeSpan = $('<span />');
-    closeSpan.text('x').addClass('w3-closebtn w3-margin-right w3-large').on('click', function(){
-        this.parentElement.style.display='none';
-    });
-
-    var emailSpan =  $('<span>').text(participantEmail);
-
-    var listPartici = $('<li />').append(emailSpan, closeSpan);
-    $('#listParticipants').append(listPartici);
+function saveParticipant() {
+	var participantEmail = $('#participantEmail').val();
+	var closeSpan = $('<span />');
+	closeSpan.text('x').addClass('w3-closebtn w3-margin-right w3-large').on('click', function() {
+		$(this.parentElement).remove();
+	});
+	var emailSpan = $('<span>').text(participantEmail);
+	var listPartici = $('<li />').append(emailSpan, closeSpan);
+	$('#listParticipants').append(listPartici);
 }
 
 function showSingleEvent(event) {
-
-    var listItem = $('<li />').text(event.name).data(event).click(showEventInfo);
-    $('#eventsListManager').append(listItem);
+	var listItem = $('<li />').text(event.name).data(event).click(showEventInfo);
+	$('#eventsListManager').append(listItem);
 }
 
-
-
 function showSingleInvitedEvent(event) {
+	var orangeButton = $('<button class="w3-btn w3-orange" />').text("Going");
+	var listItem = $('<li />').text(event.name + orangeButton).data(event).click(showEventInfo);
+	$('#invitedeventsListManager').append(listItem);
     var spacing = "          ";
     var eventStatusID = event._id;
     var maybeButton = $('<button />').addClass("w3-btn w3-orange").text("Maybe").click(function(){
@@ -296,6 +313,14 @@ function showSingleInvitedEvent(event) {
     $('#invitedeventsListManager').append(listItem);
 }
 
+function showEventInfo(oEvent) {
+	var elem = $(oEvent.target);
+	var specificEvent = elem.data();
+	var eventIdToBring = elem.data()._id;
+	$("#sideTile").fadeIn(500);
+	$("#sideName").text(specificEvent.name);
+	$("#sideDate").text(specificEvent.startDate);
+	$("#sideDateEnd").text(specificEvent.endDate);
 
 function updateStatus(statusArrival, eventStatusID){
 
@@ -337,25 +362,45 @@ function showEventInfo(oEvent){
     $("#sideDate").text(specificEvent.startDate);
     $("#sideDateEnd").text(specificEvent.endDate);
 
-    var participantsToEvent;
-    var getParticipantsAds = "/api/events/" + eventIdToBring + "/getParticipants";
-    $.ajax({
-        url: getParticipantsAds,
-        method: 'get',
-        success: function(obj) {
-            if (obj.success) {
-               participantsToEvent = obj.msg;
-                for (var i=0; i<participantsToEvent.length; ++i)
-                {
-                    $("#sideParticipants").append(participantsToEvent[i].email + '<br/>');
-                }
-            }
-        },
-        error: function(obj) {
-            $('#msgFromServer').text(obj.msg);
-        }
-    });
+	var participantsToEvent;
+	var getParticipantsAds = "/api/events/" + eventIdToBring + "/getParticipants";
+	$.ajax({
+		url: getParticipantsAds,
+		method: 'get',
+		data: {event_id: eventIdToBring},
+		success: function(obj) {
+			if (obj.success) {
+				participantsToEvent = obj.msg;
+				for (var i = 0; i < participantsToEvent.length; ++i) {
+					var eventInfoParticipant = $('<div />').addClass('participantOnEventInfo').append($('<span />').text(participantsToEvent[i].email));
+					$("#sideParticipants").append(eventInfoParticipant);
+				}
+			}
+		},
+		error: function(obj) {
+			$('#msgFromServer').text(obj.msg);
+		}
+	});
 
+	var itemsOfEvent;
+	var getItemsAds = "/api/events/" + eventIdToBring + "/getItems";
+	$.ajax({
+		url: getItemsAds,
+		method: 'get',
+		data: {event_id: eventIdToBring},
+		success: function(obj) {
+			if (obj.success) {
+				itemsOfEvent = obj.msg;
+				for (var i = 0; i < itemsOfEvent.length; ++i) {
+					var eventInfoItem = $('<div />').addClass('itemOnEventInfo').append($('<span />').text(itemsOfEvent[i].name));
+					$("#sideItems").append(eventInfoItem);
+				}
+			}
+		},
+		error: function(obj) {
+			$('#msgFromServer').text(obj.msg);
+		}
+	});
 
     var itemsOfEvent;
     var getItemsAds = "/api/events/" + eventIdToBring + "/getItems";
@@ -372,7 +417,6 @@ function showEventInfo(oEvent){
             $('#msgFromServer').text(obj.msg);
         }
     });
-
 
 }
 
